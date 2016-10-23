f505af4a86b87d59d095dc66715a601644db6710
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-10-23 13:38:29 +1000

    import socket data manually

--- client/index.jsx
+++ client/index.jsx
@@ -6,12 +6,16 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import MaterialThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import io from 'socket.io-client/socket.io';
 
 import App from './components/App';
 import store from './store';
 import * as video from './actions/video';
 
 injectTapEventPlugin();
+
+// initialise sockets
+window.socket = io();
 
 window.socket.on('chunk_received', data => {
   store.dispatch(video.addChunk(data));
